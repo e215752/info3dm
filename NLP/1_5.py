429b209d1b50eccb226ae989e0036a86b7552e89
e215752 <e215752@ie.u-ryukyu.ac.jp>
2023-08-04 15:17:20 +0900

    fixed commit

--- NLP/1_5.py
+++ NLP/1_5.py
@@ -3,5 +3,7 @@ def ngram(n, lst):
 
 str = 'I am an NLPer'
 words_bi_gram = ngram(2, str.split())
+chars_bi_gram = ngram(2, str)
 
 print('単語bi-gram:', words_bi_gram)
+print('文字bi-gram:', chars_bi_gram)
